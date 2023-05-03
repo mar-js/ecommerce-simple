e08d3d25479f79e772c8811183c09adfa36e5934
Martin <maguera37@gmail.com>
2023-05-02 21:30:03 -0300

    feat: added border bottom

--- src/UI/layouts/header/index.tsx
+++ src/UI/layouts/header/index.tsx
@@ -1,7 +1,7 @@
 import { Title } from 'UI/components'
 
 export const Header: React.FC = () => (
-  <header className="flex justify-between items-center py-2 px-6">
+  <header className="flex justify-between items-center py-2 px-6 border-t-2˝">
     <Title />
   </header>
 )
