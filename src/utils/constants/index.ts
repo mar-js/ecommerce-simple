2a691b31c6cc7a0817f4e62f93b7f5f9b7a1efbf
Martin <maguera37@gmail.com>
2023-05-04 20:39:53 -0300

    feat: created constants folder in utils

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,14 @@
+import { DataLinks } from 'interfaces'
+
+export const DATA_LINKS: DataLinks[] = [
+  {
+    id: 0,
+    text: 'Home',
+    url: '/'
+  },
+  {
+    id: 1,
+    text: 'Any',
+    url: '/'
+  }
+]
