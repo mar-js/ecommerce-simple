ae6efccb265983201719e06e40b0e759aa5c37a4
Martin <maguera37@gmail.com>
2023-05-05 20:56:27 -0300

    feat: added nav in header layout

--- src/UI/layouts/header/index.tsx
+++ src/UI/layouts/header/index.tsx
@@ -1,10 +1,14 @@
-import { Title } from 'UI/components'
 import Link from 'next/link'
+
+import { Nav } from 'UI/layouts'
+
+import { Title } from 'UI/components'
 
 export const Header: React.FC = () => (
   <header className="flex justify-between items-center py-2 px-3 border border-t-2˝">
     <Link href="/">
       <Title />
     </Link>
+    <Nav />
   </header>
 )
