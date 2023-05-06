4de36d836ac27b4f49b01529d77b399778cbdfd6
Martin <maguera37@gmail.com>
2023-05-06 18:58:42 -0300

    feat: added app template in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 
+import { TemplateApp } from 'UI'
+
 import 'styles/globals.css'
 
-const App: NextPage<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
+const App: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <TemplateApp>
+    <Component { ...pageProps } />
+  </TemplateApp>
+)
 
 export default App
