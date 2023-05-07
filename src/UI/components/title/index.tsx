c1a71278d3f7c7c23f4f521c52250febae55a2f2
Martin <maguera37@gmail.com>
2023-05-07 19:49:01 -0300

    feat: adde more size to title

--- src/UI/components/title/index.tsx
+++ src/UI/components/title/index.tsx
@@ -1,3 +1,3 @@
 export const Title: React.FC = () => (
-  <h1 className="text-xl text-green-600 hover:text-green-700">Ecommerce</h1>
+  <h1 className="text-2xl text-green-600 hover:text-green-700">Ecommerce</h1>
 )
